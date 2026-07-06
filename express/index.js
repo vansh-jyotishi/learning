ed02486662f6817bea70e2f4b31e57e3af4bf491
vansh-jyotishi <vanshjyotishi2008@gmail.com>
2026-07-07 03:54:10 +0530

    served a static public folder through express

--- express/index.js
+++ express/index.js
@@ -1,7 +1,21 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+// app.get("/getname",(req,res)=>{
+//     res.send("my name is <h1>vansh</h1>");
+// })
+// app.listen(port, () => {
+//   console.log("Example app listening on port :"+port);
+// });
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
-
+app.use(express.static("E:/learning/express/public/"))
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
