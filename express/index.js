251b695c9c898794a87d9151198f8411377141a4
vansh-jyotishi <vanshjyotishi2008@gmail.com>
2026-07-07 04:00:41 +0530

    fetched name from the query params of request and send a personalised hello message

--- express/index.js
+++ express/index.js
@@ -11,17 +11,37 @@
 // app.listen(port, () => {
 //   console.log("Example app listening on port :"+port);
 // });
+
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// const port = 3000;
+// app.use(express.static("E:/learning/express/public/"))
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+// app.get("/getname",(req,res)=>{
+//     res.send("my name is <h1>vansh</h1>");
+// })
+// app.listen(port, () => {
+//   console.log("Example app listening on port :"+port);
+// });
+
+
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = 3000;
-app.use(express.static("E:/learning/express/public/"))
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.get("/getname",(req,res)=>{
-    res.send("my name is <h1>vansh</h1>");
+
+app.get('/hello/:name2',(req,res)=>{
+    res.send("hello world by "+req.params.name2)
 })
+
 app.listen(port, () => {
   console.log("Example app listening on port :"+port);
 });
+
